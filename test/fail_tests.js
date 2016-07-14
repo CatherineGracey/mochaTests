55f246a35da71acec3813679c4cede1c77f22fa5
Catherine Gracey <cgracey@hotmail.com>
2016-07-14 18:27:07 +1000

    Added sanity checks to pass and fail files to make sure they are both running correctly.

---
+++ test/fail_tests.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Mocha', function(){
+  it('Mocha now testing failing tests.', function(){
+    expect(true).to.be.true;
+  });
+});
---
+++ test/pass_tests.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Mocha', function(){
+  it('Mocha now testing passing tests.', function(){
+    expect(true).to.be.true;
+  });
+});
